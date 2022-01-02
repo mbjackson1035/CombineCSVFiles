6961843f780b3a60fded34a1d3276149b9958120
mbjackson1035 <mbjackson1035@gmail.com>
2022-01-01 21:03:23 -0600

    Updated to reflect the following
    Different input and output dierectories
    Prints the combined file name

--- combine_cvs_files.py
+++ combine_cvs_files.py
@@ -22,8 +22,8 @@ input_directory_path = config['files']['input_directory_path']
 output_filename = config['files']['output_filename']
 
 # Variable assignments
-#input_directory_path = r"C:\Python\Dev\Test"
-#output_filename = r"C:\Python\Dev\combined.cvs"
+#input_directory_path = r"C:\Temp\Test"
+#output_filename = r"C:\Temp\combined.cvs"
 
 warning_count=0
 warning_dictionary={}
@@ -36,7 +36,7 @@ filepaths = [f for f in os.listdir(".") if f.endswith('.csv')]
 print (filepaths)
 
 if len(filepaths)==0:
-    print("\n*** Error: There are no cvs files in '%s' ***\n" % input_directory_path)
+    print("\n*** Error: There are no cvs files in directory '%s' ***\n" % input_directory_path)
     sys.exit(1)
 
 text_file = open(filepaths[1], "r")
@@ -69,13 +69,21 @@ with open(output_filename, "wb") as outfile:
                     outfile.write(os.linesep.encode("UTF-8")) 
                 else:
                     outfile.write(x)
+try:
+    startpos = output_filename.rindex("/")
+except:
+    try:
+        startpos = output_filename.rindex("\\")
+    except:
+        startpos = -1
 
+displayable_file_name=output_filename[startpos+1:]
 
 if len(warning_dictionary)==0:
-    print("\nFinished processing in %s seconds with no issues" % "{:.3f}".format(time.time() - start_time))
+    print("\nFinished processing file '{0}' in {1:.3f} seconds with no issues".format(displayable_file_name,time.time() - start_time))
 else:
     count_str=str(len(warning_dictionary))
-    print("\nFinished processing in %s seconds with %s issues" % ("{:.3f}".format(time.time() - start_time),count_str))
+    print("\nFinished processing file '{0}' in {1:.3f} seconds with {2} issues".format(displayable_file_name, time.time() - start_time, count_str))
     for key, value in warning_dictionary.items():
         print("    File '%s' %s" % (key, value))
 
